d15c636d924488ef9965b430020b5ac94d99cedf
eoozs <102992019+eoozs@users.noreply.github.com>
2022-12-01 21:47:01 +0200

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,7 @@
+# https://adventofcode.com/2022/day/1
+
+nums = [nums.split("\n") for nums in open("input.txt").read().split("\n\n")]
+sums = sorted([sum(map(int, x)) for x in nums], reverse=True)
+
+print(sums[0])
+print(sums[0] + sums[1] + sums[2])
